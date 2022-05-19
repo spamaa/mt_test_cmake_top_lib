911a09f672e2a594c1bd52cedcf356f58def4b83
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-19 13:21:58 +0200

    added dummy test for CMakeStaticLib

---
+++ tests/dummy/StaticLib/Test_AboutTest.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2010-2022 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <gtest/gtest.h>
+#include "CMakeStaticLib.h"
+
+namespace {
+    TEST(RunMethod, About)
+    {
+        ::CMakeStaticLib::About();
+    }
+}

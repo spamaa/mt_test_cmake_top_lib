6218bfb25d36d4fd4379318a2941f1ad4744c2da
Grzegorz Dymek <g_dymek@gjpdata.com>
2022-05-24 12:59:59 +0200

    boost-config 1.76#0 added

---
+++ tests/dummy/BoostConfig/Test_BoostConfigInclude.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2010-2022 The Open-Transactions developers
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <gtest/gtest.h>
+#include <boost/config.hpp>
+
+namespace {
+    TEST(RunMethod, nothing)
+    {
+        // this module defines only some configuarion macros for boost lib cmopilation,
+        // which may differ at diefferent platforms, hence I am testing just if include works
+        EXPECT_TRUE(true);
+    }
+}
